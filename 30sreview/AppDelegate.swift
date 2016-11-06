a52a61d70ce256067ead4b6e5b34e087f185ab8c
secwang <monadic.io@gmail.com>
2016-11-06 13:57:18 +0800

    Initial Commit

---
+++ 30sreview/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  30sreview
+//
+//  Created by secwang on 06/11/2016.
+//  Copyright © 2016 secwang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 30sreview/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  30sreview
+//
+//  Created by secwang on 06/11/2016.
+//  Copyright © 2016 secwang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
