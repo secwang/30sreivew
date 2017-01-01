afd013e6e1038f4fdd73db3c2d75290223b8c6fd
secwang <monadic.io@gmail.com>
2017-01-01 20:08:01 +0800

    完整的倒数逻辑

--- 30sreview/NewPostViewController.swift
+++ 30sreview/NewPostViewController.swift
@@ -17,13 +17,13 @@ class NewPostViewController: NSViewController , NSTextViewDelegate{
     var startCountDown = true
 
     @IBOutlet var mytextview: NSTextView!
-    
+
     @IBOutlet var frame: NSView!
-    
+
     @IBOutlet weak var timer: NSTextField!
-    
+
     @IBAction func sendContent(_ sender: Any) {
-        
+
         let newPost = mytextview.textStorage?.string
         print(newPost ?? "postNotExist")
         print((newPost != nil) && !(newPost?.isEmpty)!)
@@ -35,31 +35,34 @@ class NewPostViewController: NSViewController , NSTextViewDelegate{
 
         if(insertSuccess){
             mytextview.textStorage?.setAttributedString(NSAttributedString())
+            self.startCountDown = true
+            self.timer.stringValue =  String(NewPostViewController.countDown) + "S"
+            currentCountDown = NewPostViewController.countDown
         }
-        
+
         if (self.parent?.childViewControllers.count == 2){
             let tabViewContorller = self.parent as! ReviewTabViewController
             tabViewContorller.selectedTabViewItemIndex = 1
         }
 
-        
+
 
 
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         frame.layer?.backgroundColor = NSColor.white.cgColor
         self.mytextview.delegate = self
         // Do any additional setup after loading the view.
     }
-    
+
     override var representedObject: Any? {
         didSet {
             // Update the view, if already loaded.
         }
     }
-    
+
     override func viewWillAppear() {
         // Update the view, if already loaded.
     }
@@ -67,33 +70,34 @@ class NewPostViewController: NSViewController , NSTextViewDelegate{
     func textDidChange(_ notification: Notification) {
         guard (notification.object as? NSTextView) != nil else { return }
         DispatchQueue.global().async {
-            self.startCountDownFunc()
+                    self.startCountDownFunc()
         }
-   
+
     }
-    
+
     func startCountDownFunc(){
         print("hello")
         let newPost = mytextview.textStorage?.string
 
         if ((newPost != nil) && !((newPost?.isEmpty)!)){
-        if(startCountDown){
-            print("start render countdown")
-            startCountDown = false
-            currentCountDown = NewPostViewController.countDown
-            while currentCountDown > 0  {
-                print(currentCountDown)
+            if(startCountDown){
+                print("start render countdown")
+                startCountDown = false
+                currentCountDown = NewPostViewController.countDown
+                while currentCountDown > 0  {
+                    if(!startCountDown){
 
-                currentCountDown = currentCountDown - 1
-                Thread.sleep(forTimeInterval: 1)
-                print(currentCountDown)
-                DispatchQueue.main.async {
-
-                    self.timer.stringValue =  String(self.currentCountDown) + "S"
+                        currentCountDown = currentCountDown - 1
+                        Thread.sleep(forTimeInterval: 1)
+                        DispatchQueue.main.async {
+                            self.timer.stringValue =  String(self.currentCountDown) + "S"
+                        }
+                    } else{
+                        break
+                    }
                 }
-            }
 
-        }
+            }
         }
     }
 
